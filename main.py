4c59bd43d286c0f53ce956fefe32d079b2b0da1d
Luvannie <nhatanh.2552002@gmail.com>
2024-01-29 17:07:50 +0700

    data transformation

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from src.TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.TextSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion stage"
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+# STAGE_NAME = "Data Validation stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_validation = DataValidationTrainingPipeline()
+#    data_validation.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
-
-STAGE_NAME = "Data Validation stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_validation = DataValidationTrainingPipeline()
-   data_validation.main()
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -2,6 +2,7 @@ from TextSummarizer.constants.__init__ import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 from TextSummarizer.utils.common import read_yaml, create_directories
 from TextSummarizer.entity.__init__ import DataIngestionConfig
 from TextSummarizer.entity.__init__ import DataValidationConfig
+from TextSummarizer.entity.__init__ import DataTransformationConfig
 class ConfigurationManager:
     def __init__(
         self,
@@ -39,3 +40,16 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
