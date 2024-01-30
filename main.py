34f98bf4bd592c95d862727c08088c60dd2629f4
Luvannie <nhatanh.2552002@gmail.com>
2024-01-30 11:15:05 +0700

    model trainer but not enough RAM

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTra
 from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.TextSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.TextSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 # STAGE_NAME = "Data Ingestion stage"
 # try:
@@ -23,12 +24,24 @@ from src.TextSummarizer.pipeline.stage_03_data_transformation import DataTransfo
 #         logger.exception(e)
 #         raise e
 
-STAGE_NAME = "Data Transformation stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_transformation = DataTransformationTrainingPipeline()
-   data_transformation.main()
+# STAGE_NAME = "Data Transformation stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_transformation = DataTransformationTrainingPipeline()
+#    data_transformation.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
+
