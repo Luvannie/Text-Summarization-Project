7ecc41d4aa55695e1d8c0f637c791e62554779c4
Luvannie <nhatanh.2552002@gmail.com>
2024-01-30 17:24:27 +0700

    model eavluation and complete simple app using fastAPI

--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -4,6 +4,7 @@ from TextSummarizer.entity.__init__ import DataIngestionConfig
 from TextSummarizer.entity.__init__ import DataValidationConfig
 from TextSummarizer.entity.__init__ import DataTransformationConfig
 from TextSummarizer.entity.__init__ import ModelTrainerConfig
+from TextSummarizer.entity.__init__ import ModelEvaluationConfig
 class ConfigurationManager:
     def __init__(
         self,
@@ -77,3 +78,18 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+           
+        )
+
+        return model_evaluation_config
