2944c9fe9b87fc37be3b7a50ba540a1e9d32d4c2
Luvannie <nhatanh.2552002@gmail.com>
2024-01-31 16:27:45 +0700

    fix prediction

--- src/TextSummarizer/pipeline/prediction.py
+++ src/TextSummarizer/pipeline/prediction.py
@@ -1,4 +1,4 @@
-from textSummarizer.config.configuration import ConfigurationManager
+from TextSummarizer.config.configuration import ConfigurationManager
 from transformers import AutoTokenizer
 from transformers import pipeline
 
